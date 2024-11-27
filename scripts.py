2a4078bd40ea6a143c906914dfe63585f0e78531
Evgeny1337 <genya-sozykin@mail.ru>
2024-11-27 12:01:08 +0300

    bugfix

--- scripts.py
+++ scripts.py
@@ -1,53 +1,48 @@
-from datacenter.models import Schoolkid, Mark, Chastisement, Subject, Lesson
+from datacenter.models import Schoolkid, Mark, Chastisement, Subject, Lesson,Commendation
 import random
 
+
+def get_schoolkid(name):
+    return Schoolkid.objects.filter(full_name__contains=name).first()
 
 def get_random_phrase(filename):
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             phrases = file.readlines()
-            phrases = [phrase.strip() for phrase in phrases]
-            if phrases:
-                return random.choice(phrases)
-            else:
-                return
+            phrases = [phrase.strip().split('.')[1] for phrase in phrases]
+            return random.choice(phrases) if phrases else None
     except FileNotFoundError:
-        return
+        return None
 
 
 def fixed_mark(name):
-    schoolkid = Schoolkid.objects.get(full_name__contains=name)
+    schoolkid = get_schoolkid(name)
     if schoolkid:
-        bad_marks = Mark.objects.filter(
-            schoolkid=schoolkid, points__range=(0, 3))
-        for bad_mark in bad_marks:
-            bad_mark.points = 5
-            bad_mark.save()
+        Mark.objects.filter(
+            schoolkid=schoolkid, points__range=(0, 3)).update(points=5)
     else:
         print('Такого ученика несуществует')
 
 
 def remove_chastisements(name):
-    schoolkid = Schoolkid.objects.get(full_name__contains=name)
+    schoolkid = get_schoolkid(name)
     if schoolkid:
-        chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-        for chastisement in chastisements:
-            chastisement.delete()
+        Chastisement.objects.filter(schoolkid=schoolkid).delete()
     else:
         print('Такого ученика несуществует')
 
 
 def create_commendation(name, subject):
-    schoolkid = Schoolkid.objects.get(full_name__contains=name)
+    schoolkid = get_schoolkid(name)
     if (schoolkid):
-        subject = Subject.objects.filter(title=subject, year_of_study=6)[0]
+        subject = Subject.objects.filter(title=subject, year_of_study=schoolkid.year_of_study).first()
         lesson = Lesson.objects.filter(
-            year_of_study=6, group_letter='А', subject=subject).order_by('date').reverse()
-        chastisement = get_random_phrase(
-            './project/commendation.txt').split('.')[1]
-        if chastisement:
-            Chastisement.objects.create(
-                text=chastisement, created=lesson[0].date, schoolkid=schoolkid, subject=subject, teacher=lesson[0].teacher)
+            year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject=subject).order_by('-date')
+        сommendation = get_random_phrase(
+            './project/commendation.txt')
+        if сommendation:
+            Commendation.objects.create(
+                text=сommendation, created=lesson[0].date, schoolkid=schoolkid, subject=subject, teacher=lesson[0].teacher)
         else:
             print('Заполните файл commendation.txt с похвалой')
     else:
